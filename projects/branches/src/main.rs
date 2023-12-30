a864af0ea91f7b1c732cdd27f72358e17449e534
Sergij Dekht <mistersergey055@gmail.com>
2023-12-30 20:16:01 +0100

    Add receiving input from a user

--- projects/branches/src/main.rs
+++ projects/branches/src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+use std::process;
+
 fn is_polindrome(mut x: usize) -> bool {
     let mut power_of_ten = 1;
     let mut reversed = 0;
@@ -18,10 +21,36 @@ fn is_polindrome(mut x: usize) -> bool {
     reversed == x_clone
 }
 
+fn read_line_as_usize() -> usize {
+    let mut input_number = String::new();
+    
+    match io::stdin().read_line(&mut input_number) {
+        Err(error) => {
+            println!("[Error] Failed to read line: {error}.");
+            process::exit(1);
+        },
+        _ => {}
+    }
+    
+    let input_number: usize = match input_number.trim().parse() {
+        Err(error) => {
+            println!("[Error] Failed to convert the input to `usize`: {error}.");
+            process::exit(1);
+        },
+        Ok(number) => number
+    };
+
+    input_number
+}
+
 fn main() {
-    if is_polindrome(142241) {
+    println!("Please write a number.");
+
+    let input_number = read_line_as_usize();
+
+    if is_polindrome(input_number) {
         println!("Polindrome!");
-        return;
+        process::exit(0);
     }
 
     println!("Not a polindrome!!!");
